a43597e01e8ff0b08da3219eead35baa3e8ff909
xiaoming <190678@xiaoming.com>
2020-04-12 22:35:03 +0800

    优化了request方法

--- request/index.js
+++ request/index.js
@@ -1,6 +1,13 @@
 // 同时发送异步代码的次数
 let ajaxIimes = 0;
 export const request = (params) => {
+  //判断url中是否带有 /my/ 请求的是私有的路径 带上header token
+  let header = {...params.header};
+  if(params.url.includes("/my/")) {
+    // 拼接header 带上token
+    header["Authorization"] = wx.getStorageSync("token");
+  }
+
   ajaxIimes++;
   wx.showLoading({title: '加载中',mask: true})
 
@@ -9,6 +16,7 @@ export const request = (params) => {
   return new Promise((resolve, reject) => {
     wx.request({
       ...params,
+      header: header,
       url: baseUrl+params.url,
       success: (res) => {
         resolve(res.data.message);
