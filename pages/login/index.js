62c664d6cd6cedb5c3133380f05171708d68ac45
xiaoming <190678@xiaoming.com>
2020-04-14 20:10:28 +0800

    增加了用户登录

---
+++ pages/login/index.js
@@ -0,0 +1,12 @@
+// pages/login/index.js
+Page({
+  // 获取用户信息
+  handleGetUserInfo(e) {
+    const {userInfo} = e.detail;
+    wx.setStorageSync("userInfo",userInfo);
+    wx.navigateBack({
+      delta: 1
+    });
+  }
+  
+})
